1455595c163041cbaf9baacd7b962f3468a9c198
tornermarton <torner.marton@gmail.com>
2019-07-11 09:46:31 +0200

    Evaluation helper methods added

---
+++ jalef/evaluation.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import confusion_matrix
+
+from sklearn import metrics
+
+__all__ = [
+    "plot_confusion_matrix",
+    "evaluate_result"
+]
+
+
+def plot_confusion_matrix(y_true, y_pred,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.get_cmap("Blues"),
+                          show_fig=True,
+                          save_fig=False,
+                          filename=""):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = unique_labels(y_true)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+
+    if save_fig:
+        plt.savefig(filename, dpi="figure", bbox_inches="tight")
+
+    if show_fig:
+        plt.show()
+
+
+def evaluate_result(y_true, y_pred):
+    accuracy = metrics.accuracy_score(y_true, y_pred)
+    precision = metrics.precision_score(y_true, y_pred, average='weighted')
+    recall = metrics.recall_score(y_true, y_pred, average='weighted')
+    f1_score = metrics.f1_score(y_true, y_pred, average='weighted')
+
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1_score": f1_score}
