986ecd32d7591856f85bfe786577b7ce028b6869
tornermarton <torner.marton@gmail.com>
2019-10-03 15:50:21 +0200

    Docker container updated

---
+++ docker/run.sh
@@ -0,0 +1,1 @@
+docker run --runtime=nvidia -e LOCAL_USER_ID=`id -u $USER` -e LOCAL_GROUP_ID=`id -g $USER` --name 'tm-jalef' -p 7788:8888 -p 7706:6006 -p 7722:22 -v $(pwd):/app tornermarton/jalef:latest
