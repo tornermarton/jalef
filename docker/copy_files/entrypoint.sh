297b0db7c5cf312cb7fadce340dd367fd3d552e6
tornermarton <torner.marton@gmail.com>
2019-11-06 15:38:02 +0100

    Docker working

--- docker/copy_files/entrypoint.sh
+++ docker/copy_files/entrypoint.sh
@@ -12,10 +12,13 @@ echo "Starting with UID : $USER_ID"
 groupadd -f -g $GROUP_ID $USER_NAME
 useradd --shell /bin/bash -u $USER_ID -g $GROUP_ID -o -c "" -m $USER_NAME
 adduser $USER_NAME sudo
-echo '$USER_NAME:nehezjelszo' | chpasswd
+echo $USER_NAME':nehezjelszo' | chpasswd
 export HOME=/home/$USER_NAME
+mkdir /home/$USER_NAME/.ssh
 
 less /root/copy_files/bashrc_append.txt >> /root/.bashrc
 less /root/copy_files/bashrc_append.txt >> /home/$USER_NAME/.bashrc
+
+chown $USER_NAME:$USER_NAME /home/$USER_NAME/ -R
 
 /usr/bin/supervisord
